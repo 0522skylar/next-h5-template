4f14588fe4eb331b04ed0cdf658af94a1f1b05b4
huanghaiyan <2031499619@qq.com>
2023-04-16 12:16:15 +0800

    feat: add sass

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ pnpm dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
+
+## 项目配置
+1.eslint
+2.prettier
+3.husky
+
---
+++ pages/about/index.tsx
@@ -0,0 +1,8 @@
+import styles from './index.module.scss'
+export default function About() {
+
+
+  return <>
+    <div className={styles.wrap}>1234</div>
+  </>
+}
