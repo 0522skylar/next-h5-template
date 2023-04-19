0b03481d46cd3f6797095c41f285419b7b8e2682
lightskyblue <2031499619@qq.com>
2023-04-19 11:17:59 +0800

    feat: add commit rule

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  extends: ['next/core-web-vitals'],
+  env: {},
+  globals: {
+    jsx: true,
+    global: true
+  },
+  plugins: ['prettier'],
+  
+  rules: {
+    quotes: ['error', 'single'],
+    'prefer-const': 1,
+    'no-duplicate-imports': 0,
+    'array-bracket-spacing': 0,
+    '@typescript-eslint/no-this-alias': 1,
+    '@typescript-eslint/ban-types': 0,
+    'react/no-unknown-property': 0,
+  },
+};
