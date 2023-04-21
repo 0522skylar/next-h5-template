d98cddbd8ea60cdbccb35d9183fd1da95f3a52a8
huanghaiyan <2031499619@qq.com>
2023-04-21 09:20:52 +0800

    style: add color

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    prependData: `
+    @import "@styles/color.scss";
+    `,
+    includePaths: [
+      path.join(__dirname, 'styles'),
+      path.join(__dirname, 'pages'),
+    ],
+  },
 }
 
 module.exports = nextConfig
